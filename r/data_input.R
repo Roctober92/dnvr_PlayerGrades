915a372ed9631d6fe73d649fe5e01b0005174e0d
Andrew Wolfe <wolfe.andrew@gmail.com>
2020-10-17 08:35:33 -0600

    insert
     game 4

--- r/data_input.R
+++ r/data_input.R
@@ -14,9 +14,13 @@ p_load(tidyverse, ggplot2)
 # game, author, player, grade, gametype, date
 game <- c(1:50)
 
-date <- c('2019-10-03', '2019-10-05')
+plus_minus <- c()
 
-author <- c('Rawal', 'Haefele'))
+total_goals <- c()
+
+date <- c('2019-10-03','2019-10-05','2019-10-10', '2019-10-12')
+
+author <- c('Rawal','Haefele','Rawal','Rawal')
 
 gametype <- c(rep('reg', 50))
 
@@ -33,6 +37,34 @@ gm1 <- tibble(
   grade = c('c+', 'c', 'b', 'b+', 'c+', 'b-', 'c+', 'c', 'b-', 'b-', 'c-', 'b', 'b+', 'a-', 'b', 'b', 'b-', 'a', 'c'),
   player_id = c(17, 2, 13, 10, 18, 14, 35, 11, 1, 9, 15, 7, 8, 16, 6, 4, 3, 5, 32)
 )
+
+gm2 <- tibble(
+  game = c(rep(2, 19)),
+  grade = c('a', 'a-', 'a+', 'c+', 'b-', 'c+', 'c+', 'c', 'b-', 'c+', 'b+', 'b+', 'b+', 'b-', 'c+', 'b', 'b+', 'c-', 'b+'),
+  player_id = c(4,1,5,14,6,8,16,9,3,10,15,13,7,17,18,2,11,35,32)
+)
+
+gm3 <- tibble(
+  game = c(rep(3, 19)),
+  grade = c('c-','c','b','d','c','b','b','b-','b-','b','b','b+','b','b-','b-','b','a-','b','a'),
+  player_id = c(17,2,13,10,18,14,21,11,1,15,24,7,8,16,6,4,3,5,32)
+)
+
+gm4 <- tibble(
+  game = c(rep(4,19)),
+  grade = c('b','b+','d','d','d','b+','d','b-','c-','d','d','b','c+','b+','b-','c','b+','b-','b+'),
+  player_id = c(17,2,13,10,18,14,21,11,1,15,24,7,8,16,6,4,3,5,31)
+  
+)
+
+
+
+
+
+
+
+
+
 
 
 
